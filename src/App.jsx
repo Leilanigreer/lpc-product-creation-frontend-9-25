80e3a7d3f674805375960c2c654019a9008c9c87
Leilanigreer <leilani@lpcgolf.com>
2024-09-26 14:48:59 -0700

    inital setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { HeadcoversPage } from "./HeadcoversPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeadcoversPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/HeadcoversPage.jsx
@@ -0,0 +1,7 @@
+export function HeadcoversPage() {
+  return (
+    <main>
+      <h1>Welcome to the start of our app!</h1>
+    </main>
+  )
+}
